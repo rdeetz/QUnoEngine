8bc08186f6a034877d44a1733ef6ad1a2b2d037f
Roger Deetz <rdeetz@gmail.com>
2021-01-08 14:36:39 -0500

    Skeleton of a new beast.

---
+++ QUnoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QUnoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
